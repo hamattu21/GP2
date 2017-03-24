77b617973fc312043774771d3c4b0ae69d8cd832
Mohammed <mmalmoutairi@gmail.com>
2017-03-24 21:30:55 +0300

    Database

---
+++ db/db.php
@@ -0,0 +1,28 @@
+<?php
+
+    // set parameter for connection database
+    $server = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'GP2';
+
+    //connect to database
+    $connection = @mysqli_connect($server, $username, $password);
+
+    //if not connect to database print error mesaage
+    if (!$connection) {
+        die("can not connect to Server");
+    }
+
+    //select database
+    $db = mysqli_select_db($connection, $database);
+
+    //if database not exists print error message
+    if (!$db) {
+        die("can not connect to Database");
+    }
+
+    // set utf8 for arabic letter
+    mysqli_query($connection, "SET NAMES 'utf8'");
+
+?>
